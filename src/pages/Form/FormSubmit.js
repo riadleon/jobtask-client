3380bb3dad86d081709b4981aa75e0026f612d52
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2023-01-02 16:17:40 +0600

    some change up in the ui

--- src/pages/Form/FormSubmit.js
+++ src/pages/Form/FormSubmit.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import bg from '../../assets/bg.png';
+ 
 import './FormSubmit.css'
 
 const FormSubmit = () => {
     return (
         <div className="bg min-h-screen flex items-center text-lg p-48" >
-            <h3 className='text-2xl'>Please enter your name <br /> <span>and pick the Sectors <span><br />
+            <h3 className='text-2xl'>Please enter your <span className='text-primary'>Name</span> <br /> <span>and pick the <span className='text-secondary'>Sectors</span> <span><br />
                 you are currently involved in.</span> </span> </h3>
             <form action="" className="p-10 md:w-2/3 lg:w-1/2 mx-auto rounded">
                 <div className="shadow">
@@ -13,7 +13,7 @@ const FormSubmit = () => {
                         <label for="name" className="w-20 text-right mr-8 p-4 text-purple-200">Name:</label>
                         <input type="text" name="name" id="name" placeholder="Put in your name" className="flex-1 p-4 pl-0 bg-transparent placeholder-purple-300  outline-none text-white overflow-ellipsis overflow-hidden" />
                     </div>
-                    <div className="flex items-center  rounded-t-lg border-purple-500 border-b">
+                    <div className="flex items-center  rounded-t-lg border-purple-500 border-b mb-8">
                         <label for="name" className="w-20 text-right mr-8 p-4 text-purple-200">Sectors:</label>
                         <select className="flex-1 p-4 pl-0 bg-transparent placeholder-purple-300  outline-none  overflow-ellipsis overflow-hidden">
                             <option disabled selected>Pick your Sectors</option>
