1417e8f38dc12c9f3901fa4d24d18d5f0a587c6d
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2023-01-04 02:34:12 +0600

    data stored on the database

--- src/pages/Form/FormSubmit.js
+++ src/pages/Form/FormSubmit.js
@@ -4,11 +4,52 @@ import '@mobiscroll/react/dist/css/mobiscroll.min.css';
 // import Select from "react-select";
 import './FormSubmit.css'
 import { useLoaderData } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
 
 
 const FormSubmit = () => {
     const myData = useLoaderData([]);
-    console.log(myData);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+
+        const name = form.name.value;
+        const sectors = form.sectors.value;
+        const checked = form.checked.value;
+
+
+        const storeData = {
+            clientName: name,
+            sectors,
+            checked
+
+        }
+
+
+        fetch('http://localhost:8000/storing', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(storeData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.acknowledged) {
+
+                    toast.success('Data Stored confirmed');
+
+                }
+                else {
+                    toast.error(data.message);
+                }
+            })
+
+
+    }
 
 
 
@@ -16,7 +57,7 @@ const FormSubmit = () => {
         <div className="bg min-h-screen flex items-center text-lg p-48" >
             <h3 className='text-2xl'>Please enter your <span className='text-primary'>Name</span> <br /> <span>and pick the <span className='text-secondary'>Sectors</span> <span><br />
                 you are currently involved in.</span> </span> </h3>
-            <form action="" className="p-10 md:w-2/3 lg:w-1/2 mx-auto rounded">
+            <form onSubmit={handleSubmit} className="p-10 md:w-2/3 lg:w-1/2 mx-auto rounded">
                 <div className="shadow">
                     <div className="flex items-center bg-transparent rounded-t-lg border-purple-500 border-b">
                         <label for="name" className="w-20 text-right mr-8 p-4 text-purple-200">Name:</label>
@@ -27,15 +68,16 @@ const FormSubmit = () => {
                         <Select
                             data={myData}
                             touchUi={false}
-                            name={myData.value}
+                            name="sectors"
                             className="flex-1 p-4 pl-0 bg-transparent placeholder-purple-300  outline-none  overflow-ellipsis overflow-hidden" >
 
                         </Select>
 
+
                     </div>
                     <div class="mb-4">
                         <label class="block">
-                            <input className="w-20 text-right mr-8 p-4 text-purple-200" type="checkbox" />
+                            <input name='checked' className="w-20 text-right mr-8 p-4 text-purple-200" type="checkbox" />
                             <span class="text-lg text-purple-200">
                                 Agree to terms
                             </span>
@@ -43,7 +85,7 @@ const FormSubmit = () => {
                     </div>
 
                 </div>
-                <button className=" btn-primary block w-full rounded py-4 text-white font-bold shadow">Submit</button>
+                <input type="submit" value='submit' className=" btn-primary block w-full rounded py-4 text-white font-bold shadow" />
 
             </form>
         </div>
