52e5612bd6ef5be9c723d4a5b20cf97d4057a0e9
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2023-01-05 22:16:48 +0600

    inserting solved with validation

--- src/pages/Form/FormSubmit.js
+++ src/pages/Form/FormSubmit.js
@@ -11,92 +11,98 @@ import { toast } from 'react-hot-toast';
 const FormSubmit = () => {
     const myData = useLoaderData([]);
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        const form = event.target;
-
-        const name = form.name.value;
-        const sectors = form.sectors.value;
-        const checked = form.checked.value;
+    const { register, handleSubmit, resetField, rest, formState: { errors } } = useForm();
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
 
-        const storeData = {
-            clientName: name,
-            sectors,
-            checked
-
-        }
-
-
+    const onSubmitHandler = user => {
         fetch('http://localhost:8000/storing', {
             method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(storeData)
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data.acknowledged) {
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(user)
+        }).then(() => {
+            console.log(`${user.name} ${user.sectors} is inserted successfully!!!`);
+            toast(`${user.name} updated ${user.sectors} in to the form!!!`);
+            resetField("name");
+            resetField("sectors");
+            resetField("checked");
+            setErrorMessage('');
+        }).catch(error => {
+            console.error(error.message);
+            toast('Insert Data failed!!!');
+            setMessage('');
+        });
+    };
 
-                    toast.success('Data Stored confirmed');
-
-                }
-                else {
-                    toast.error(data.message);
-                }
-            })
-
-
-    }
-
+    const onFocusHandler = () => {
+        setMessage('');
+        setErrorMessage('');
+    };
 
 
     return (
-        <div className="bg min-h-screen flex items-center text-lg p-48" >
-            <h3 className='text-2xl'>Please enter your <span className='text-primary'>Name</span> <br /> <span>and pick the <span className='text-secondary'>Sectors</span> <span><br />
-                you are currently involved in.</span> </span> </h3>
-            <form onSubmit={handleSubmit} className="p-10 md:w-2/3 lg:w-1/2 mx-auto rounded">
-                <div className="shadow">
-                    <div className="flex items-center bg-transparent rounded-t-lg border-purple-500 border-b">
-                        <label for="name" className="w-20 text-right mr-8 p-4 text-purple-200">Name:</label>
-                        <input required type="text" name="name" id="name" className="flex-1 p-4 pl-0 bg-transparent placeholder-purple-300  outline-none text-white overflow-ellipsis overflow-hidden" />
-
+        <>
+            <div className="bg min-h-screen flex items-center text-lg p-48" >
+                <h3 className='text-2xl'>Please enter your <span className='text-primary'>Name</span> <br /> <span>and pick the <span className='text-secondary'>Sectors</span> <span><br />
+                    you are currently involved in.</span> </span> </h3>
+                <form className="p-10 md:w-2/3 lg:w-1/2 mx-auto rounded" onSubmit={handleSubmit(onSubmitHandler)}>
+                    <div className="shadow">
+                        <div className="flex items-center bg-transparent rounded-t-lg border-purple-500 border-b">
+                            <label htmlFor="name" className="w-20 text-right mr-8 p-4 text-purple-200">Name:</label>
+                            <input type='text' onFocus={onFocusHandler} {...register('name', {
+                                required: "Name is Required"
+                            })} className="flex-1 p-4 pl-0 bg-transparent placeholder-purple-300  outline-none text-white overflow-ellipsis overflow-hidden" /><br />
+                            {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                        </div>
                     </div>
                     <div className="flex items-center  rounded-t-lg border-purple-500 border-b mb-8 ">
-                        <label for="sectors" className="w-20 text-right mr-8 p-4 text-purple-200">Sectors:</label>
-                        <Select
-                            data={myData}
-                            touchUi={false}
-                            // error={true} errorMessage="Must Choose One"
-                            name="sectors"
-                            className="flex-1 p-4 pl-0 bg-transparent placeholder-purple-300  outline-none  overflow-ellipsis overflow-hidden" >
+                        <label htmlFor="sectors" className="w-20 text-right mr-8 p-4 text-purple-200">Sectors:</label>
+                        <select className="flex-1 p-4 pl-0 bg-transparent placeholder-purple-300  outline-none  overflow-ellipsis overflow-hidden" required onFocus={onFocusHandler} {...register("sectors")} {...rest}>
+                            {
+                                myData.map(data =>
 
-                        </Select>
+                                    <option className="text-purple-500 flex-1 p-4 pl-0 bg-transparent placeholder-purple-300  outline-none  overflow-ellipsis overflow-hidden" value={data.value}>{data.text}</option>
+
+                                )
+                            }
+                        </select>
 
 
                     </div>
                     <div class="mb-4">
                         <label class="block">
-                            <input required name='checked' className="w-20 text-right mr-8 p-4 text-purple-200" type="checkbox" />
+                            <input required name='checked' className="w-20 text-right mr-8 p-4 text-purple-200" type="checkbox" onFocus={onFocusHandler} {...register('checked')} />
                             <span class="text-lg text-purple-200">
                                 Agree to terms
                             </span>
                         </label>
                     </div>
+                    {/* <Link to='/edit'>
+                        <input type="submit" value='submit' className=" btn-primary block w-full rounded py-4 text-white font-bold shadow" />
+                    </Link> */}
 
-                </div>
-                <Link to='/edit'>
-                    <input type="submit" value='Save' className=" btn-primary block w-full rounded py-4 text-white font-bold shadow" />
-                </Link>
-
-            </form>
-        </div>
+                    <button className=" btn-primary block w-full rounded py-4 text-white font-bold shadow" type="submit">Submit</button>
+                    {/* <Link to={"/"}><button>Display</button></Link> */}
+                </form>
+                <p style={{ color: "pink" }}>{message}</p>
+                <p style={{ color: "orange" }}>{errorMessage}</p>
+            </div>
+        </>
     );
 };
 
 export default FormSubmit;
+
+
+
+
+
+
+
+
+
+
 
 
 
